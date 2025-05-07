cb6474b7b42bf96a17d72c472e90894926cdcf53
FaridAlieff <farid.alieff@gmail.com>
2025-05-07 21:05:18 +0400

    edited

--- next.config.js
+++ next.config.js
@@ -7,3 +7,27 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 module.exports = withBundleAnalyzer({
   reactStrictMode: true,
 });
+
+// next.config.js
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: "default-src * 'self' blob: data: 'unsafe-inline' 'unsafe-eval'; frame-ancestors *; script-src * 'unsafe-inline' 'unsafe-eval'",
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'ALLOWALL',
+  },
+];
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
